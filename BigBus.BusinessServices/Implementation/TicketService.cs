a65cd2a5f280112a01093b41db91dfd0cc5311d4
unknown <admin@cyberminds.co.uk>
2015-12-20 19:55:12 +0000

    Added data layer

---
+++ BigBus.BusinessServices/Implementation/TicketService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BigBus.BusinessServices.Infrastructure;
+using BigBus.DataServices;
+using BigBus.DataServices.Repositories.Infrastructure;
+
+namespace BigBus.BusinessServices.Implementation
+{
+    public class TicketService: ITicketService
+    {
+        private IGenericDataRepository<tb_Ticket> _ticketRepos;
+
+        public TicketService(IGenericDataRepository<tb_Ticket> ticketRepository)
+        {
+            _ticketRepos = ticketRepository;
+        }
+    }
+}
